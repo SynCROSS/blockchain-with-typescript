37c3f7938ed398bdc99b2c6b2a710bde4f52f0af
SynCROSS <kuuhaku3125@gmail.com>
2022-05-15 14:36:01 +0900

    Refactor Code

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,18 @@
-import * as CryptoJS from 'C:/Users/kuuha/AppData/Local/Yarn/Data/global/node_modules/crypto-js';
+import CryptoJS from 'crypto-js';
 
 class Block {
-  private index: number;
-  private hash: string;
-  private prevHash: string;
-  private data: string;
-  private timestamp: number;
-
-  static calcBlockHash = (
-    index: number,
-    prevHash: string,
-    timestamp: number,
-    data: string,
-  ): string => CryptoJS.SHA256(index + prevHash + timestamp + data).toString();
-
-  static validateStructure = (block: Block): boolean =>
-    typeof block.getIndex() === 'number' &&
-    typeof block.getHash() === 'string' &&
-    typeof block.getPrevHash() === 'string' &&
-    typeof block.getTimestamp() === 'number' &&
-    typeof block.getData() === 'string';
+  private readonly index: number;
+  private readonly hash: string;
+  private readonly prevHash: string;
+  private readonly data: string;
+  private readonly timestamp: number;
 
   constructor(
-    index: number,
-    hash: string,
-    prevHash: string,
-    data: string,
-    timestamp: number,
+      index: number,
+      hash: string,
+      prevHash: string,
+      data: string,
+      timestamp: number,
   ) {
     this.index = index;
     this.hash = hash;
@@ -35,83 +21,121 @@ class Block {
     this.timestamp = timestamp;
   }
 
-  public getIndex = () => {
+  static calcBlockHash = (
+      index: number,
+      prevHash: string,
+      timestamp: number,
+      data: string,
+  ): string => CryptoJS.SHA256(index + prevHash + timestamp + data)
+                       .toString();
+
+  static validateStructure = (block: Block): boolean =>
+      typeof block.getIndex() === 'number' &&
+      typeof block.getTimestamp() === 'number' &&
+      typeof block.getHash() === 'string' &&
+      typeof block.getPrevHash() === 'string' &&
+      typeof block.getData() === 'string';
+
+  public getIndex() {
     return this.index;
   };
-  public getHash = () => {
+
+  public getHash() {
     return this.hash;
   };
-  public getPrevHash = () => {
+
+  public getPrevHash() {
     return this.prevHash;
   };
-  public getData = () => {
+
+  public getData() {
     return this.data;
   };
-  public getTimestamp = () => {
+
+  public getTimestamp() {
     return this.timestamp;
   };
+
 }
 
-const genesisBlock: Block = new Block(0, 'hash', '', 'data', Date.now());
+const now = Date.now();
+const genesisBlock: Block = new Block(0, 'hash', '', 'data', now);
 
 let blockChain: Block[] = [genesisBlock];
 const getBlockChain = (): Block[] => blockChain;
 
 const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
 
-const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+const today = new Date;
+const time = today.getTime();
+const getNewTimestamp = (): number => Math.round(time / 1000);
 
 const createNewBlock = (data: string): Block => {
   const prevBlock: Block = getLatestBlock();
+  const prevHash = prevBlock.getHash();
+
   const newIndex: number = prevBlock.getIndex() + 1;
   const newTimestamp: number = getNewTimestamp();
   const newHash: string = Block.calcBlockHash(
-    newIndex,
-    prevBlock.getHash(),
-    newTimestamp,
-    data,
+      newIndex,
+      prevHash,
+      newTimestamp,
+      data,
   );
   const newBlock: Block = new Block(
-    newIndex,
-    newHash,
-    prevBlock.getHash(),
-    data,
-    newTimestamp,
+      newIndex,
+      newHash,
+      prevHash,
+      data,
+      newTimestamp,
   );
+
   addBlock(newBlock);
   return newBlock;
 };
 
-const getHashforBlock = (block: Block): string =>
-  Block.calcBlockHash(
-    block.getIndex(),
-    block.getPrevHash(),
-    block.getTimestamp(),
-    block.getData(),
+const getHashForBlock = (block: Block): string => {
+  const index = block?.getIndex() ?? 0;
+  const prevHash = block?.getPrevHash() ?? '';
+  const timestamp = block?.getTimestamp() ?? 0;
+  const data = block?.getData() ?? '';
+
+  return Block.calcBlockHash(
+      index,
+      prevHash,
+      timestamp,
+      data,
   );
+};
 
 const isBlockValid = (candidateBlock: Block, prevBlock: Block): boolean => {
   if (!Block.validateStructure(candidateBlock)) {
     return false;
-  } else if (prevBlock.getIndex() + 1 !== candidateBlock.getIndex()) {
-    return false;
-  } else if (prevBlock.getHash() !== candidateBlock.getPrevHash()) {
-    return false;
-  } else if (candidateBlock.getHash() !== getHashforBlock(candidateBlock)) {
-    return false;
-  } else {
-    return true;
   }
+
+  if (prevBlock.getIndex() + 1 !== candidateBlock.getIndex()) {
+    return false;
+  }
+
+  if (prevBlock.getHash() !== candidateBlock.getPrevHash()) {
+    return false;
+  }
+
+  return candidateBlock.getHash() === getHashForBlock(candidateBlock);
 };
 
 const addBlock = (candidateBlock: Block): void => {
-  if (isBlockValid(candidateBlock, getLatestBlock())) {
+  const latestBlock = getLatestBlock();
+
+  if (isBlockValid(candidateBlock, latestBlock)) {
     blockChain.push(candidateBlock);
   }
 };
+
 createNewBlock('data1');
 createNewBlock('data2');
 createNewBlock('data3');
+
 console.log(blockChain);
 
 export {}; // * export on TS
